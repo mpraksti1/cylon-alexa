192f0f8d1ad6c6bdba285cea07f18f1f9b3520da
Mike Praksti <mike.praksti@rockfishinteractive.com>
2016-11-18 16:12:34 -0500

    servo

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ var Cylon = require("Cylon");
 
 Cylon.robot({
   connections: {
-    server: { adaptor: 'mqtt', host: 'mqtt://broker.hivemq.com' }
+    server: { adaptor: 'mqtt', host: 'mqtt://broker.hivemq.com' },
+    raspi: { adaptor: 'raspi' }
   },
+
+  devices: {
+    servo: { driver: 'servo', pin: 11 }
+  }
 
   work: function(my) {
     my.server.subscribe('rules');
 
     my.server.on('message', function (topic, data) {
-      console.log(topic + ": " + data);
+      var angle = 30,
+          increment = 40;
+
+      every((1).seconds(), function() {
+        angle += increment;
+        my.servo.angle(angle);
+        console.log("Current Angle: " + (my.servo.currentAngle()));
+
+        if ((angle === 30) || (angle === 150)) { increment = -increment; }
+      });
     });
   }
 }).start();
